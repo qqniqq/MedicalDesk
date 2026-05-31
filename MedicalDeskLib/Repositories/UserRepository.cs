0e290cfe6c38744709d84d826af20cd2c1b1a15b
Саид Мужаидов <69said05@gmail.com>
2026-06-01 01:10:09 +0300

    сделал хэш пароля для админа

---
+++ MedicalDeskLib/Repositories/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using MedicalDeskLib.Data;
+using MedicalDeskLib.Models;
+using MySql.Data.MySqlClient;
+
+namespace MedicalDeskLib.Repositories
+{
+    public class UserRepository
+    {
+        public List<User> GetAll()
+        {
+            List<User> users = new List<User>();
+
+            using (MySqlConnection connection =
+                DbConnectionFactory.CreateConnection())
+            {
+                connection.Open();
+
+                string query = "SELECT * FROM Users";
+
+                MySqlCommand command =
+                    new MySqlCommand(query, connection);
+
+                MySqlDataReader reader =
+                    command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    User user = new User();
+
+                    user.Id =
+                        reader.GetInt32("Id");
+
+                    user.Login =
+                        reader.GetString("Login");
+
+                    user.FullName =
+                        reader.GetString("FullName");
+
+                    user.Role =
+                        reader.GetInt32("Role");
+
+                    user.IsActive =
+                        reader.GetBoolean("IsActive");
+
+                    users.Add(user);
+                }
+            }
+            return users;
+        }
+    }
+}
---
+++ MedicalDeskLib/Security/PasswordHasher.cs
@@ -0,0 +1,21 @@
+﻿using BCrypt.Net;
+
+namespace MedicalDeskLib.Security
+{
+    public static class PasswordHasher
+    {
+        public static string Hash(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool Verify(
+            string password,
+            string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(
+                password,
+                hash);
+        }
+    }
+}
