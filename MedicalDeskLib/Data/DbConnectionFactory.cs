a8f28746cbd38d0052414adcef0217b424f075b6
Саид Мужаидов <69said05@gmail.com>
2026-05-31 22:29:03 +0300

    подключение к бд

---
+++ MedicalDeskLib/Data/DbConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace MedicalDeskLib.Data
+{
+    public static class DbConnectionFactory
+    {
+        private static readonly string ConnectionString =
+            "server=localhost;" +
+            "database=medicaldesk;" +
+            "uid=root;" +
+            "pwd=vertrigo;" +
+            "charset=utf8mb4;";
+
+        public static MySqlConnection CreateConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
