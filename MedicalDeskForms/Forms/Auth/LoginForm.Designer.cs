e4960733c279a070890630385d2ca9042f092e17
Саид Мужаидов <69said05@gmail.com>
2026-06-01 01:34:41 +0300

    создал форму регистрации

--- MedicalDeskForms/Forms/Auth/LoginForm.Designer.cs
+++ MedicalDeskForms/Forms/Auth/LoginForm.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.lblError = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblLogin.Location = new System.Drawing.Point(155, 91);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(59, 20);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "Логин";
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtLogin.Location = new System.Drawing.Point(221, 91);
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(136, 27);
+            this.txtLogin.TabIndex = 1;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPassword.Location = new System.Drawing.Point(142, 141);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(72, 20);
+            this.lblPassword.TabIndex = 2;
+            this.lblPassword.Text = "Пароль";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtPassword.Location = new System.Drawing.Point(221, 141);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(136, 27);
+            this.txtPassword.TabIndex = 3;
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnLogin.Location = new System.Drawing.Point(221, 191);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(136, 39);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Войти";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblError
+            // 
+            this.lblError.AutoSize = true;
+            this.lblError.ForeColor = System.Drawing.Color.Red;
+            this.lblError.Location = new System.Drawing.Point(267, 242);
+            this.lblError.Name = "lblError";
+            this.lblError.Size = new System.Drawing.Size(44, 16);
+            this.lblError.TabIndex = 5;
+            this.lblError.Text = "label1";
+            this.lblError.Visible = false;
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(539, 337);
+            this.Controls.Add(this.lblError);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.lblLogin);
+            this.Name = "LoginForm";
             this.Text = "LoginForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Label lblError;
     }
 }
---
+++ MedicalDeskLib/Services/AuthService.cs
@@ -0,0 +1,69 @@
+﻿using MedicalDeskLib.Data;
+using MedicalDeskLib.Models;
+using MedicalDeskLib.Security;
+using MySql.Data.MySqlClient;
+
+namespace MedicalDeskLib.Services
+{
+    public class AuthService
+    {
+        public User Login(
+            string login,
+            string password)
+        {
+            using (var connection =
+                DbConnectionFactory.CreateConnection())
+            {
+                connection.Open();
+
+                string query =
+                    @"SELECT * FROM Users
+                      WHERE Login=@Login
+                      AND IsActive=1";
+
+                MySqlCommand command =
+                    new MySqlCommand(
+                        query,
+                        connection);
+
+                command.Parameters.AddWithValue(
+                    "@Login",
+                    login);
+
+                MySqlDataReader reader =
+                    command.ExecuteReader();
+
+                if (!reader.Read())
+                    return null;
+
+                string hash =
+                    reader["PasswordHash"]
+                    .ToString();
+
+                bool valid =
+                    PasswordHasher.Verify(
+                        password,
+                        hash);
+
+                if (!valid)
+                    return null;
+
+                User user = new User();
+
+                user.Id =
+                    (int)reader["Id"];
+
+                user.Login =
+                    reader["Login"].ToString();
+
+                user.FullName =
+                    reader["FullName"].ToString();
+
+                user.Role =
+                    (int)reader["Role"];
+
+                return user;
+            }
+        }
+    }
+}
